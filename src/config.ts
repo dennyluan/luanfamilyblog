896d59bc554b5bbaed406907c4c4b2f69762dcae
denny <dennyluan@gmail.com>
2025-08-17 22:08:26 -1000

    Update config

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
-    description: 'Minimal blog built by Astro', // Site description
+    website: 'https://luan.family/', // Site domain
+    title: 'Luan Family', // Site title
+    author: 'Denny & Cindy', // Author name
+    description: 'A family blog', // Site description
     language: 'en-US' // Default language
   },
 
