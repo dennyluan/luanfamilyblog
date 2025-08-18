ce8fffb302c7f498a57a9a22338730bb3891a41d
denny <dennyluan@gmail.com>
2025-08-17 21:14:41 -1000

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({
-  site: "https://luan.family",
-  trailingSlash: "always",
+  site: "https://luan.family"
 });
